c49e5dddb831f3ad565495799ea8bbc29a60776b
p3d3r2 <kleppepeder@gmail.com>
2026-01-29 11:56:12 +0100

    js

--- script.js
+++ script.js
@@ -3,13 +3,36 @@ const display = document.querySelector('#display');
 async function fetchData() {
   console.log('This is fetchData function');
 
+  // Setter opp en variabel som henter data fra en URL
   const data = await fetch('https://catfact.ninja/facts');
-  return;
+  console.log(data);
+
+  // Setter opp en variabel som gjør dataen vi hentet lesbar
+  const dataParsed = data.json();
+  console.log(dataParsed);
+
+  // Siden vi bruker to funksjoner må bi returnere variabelen vi vil ta med videre
+  return dataParsed;
 }
 
 async function displayData() {
+  // Siden vi bruker to funksjoner må vi ta imot den første funksjonen først
   const response = await fetchData();
   console.log(response);
+  console.log(response.data);
+  console.log(response.data[0]);
+  console.log(response.data[0].fact);
+
+
+  // Setter opp en variabel som forkorter koden vi må skrive
+  const catFact = response.data;
+
+  // Setter opp en forEach som "looper" gjennom (index) og gjør koden i (curly-brackets) like mange ganger som der er object i array
+ catFact.forEach((item) => {
+    const displayItem = document.createElement("li");
+    displayItem.textContent = item.fact;
+    display.appendChild(displayItem);
+  });
 }
 
 displayData();
